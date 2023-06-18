41964cd65f6562849262ccfe61298e09ee67b105
Olimpiu Seulean <seulean.olimpiu@gmail.com>
2023-06-18 22:18:57 +0300

    transition to yarn, added husky, and configured linter properly.

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  extends: [
+    'eslint:recommended',
+    'plugin:vue/vue3-strongly-recommended', // This option imposes formatting rules on your code to improve readability
+  ],
+  env: {
+    browser: true,
+    node: true,
+    es2020: true,
+  },
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: [
+    'vue',
+  ],
+  rules: {
+    quotes: [2, 'single', { 'avoidEscape': true }]
+  }
+}
--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,12 @@
 <script setup>
-import { items } from "./movies.json";
-/*
- This is an Icon that you can use to represent the stars if you like
- otherwise you could just use a simple ⭐️ emoji, or * character.
-*/
-// import { StarIcon } from "@heroicons/vue/24/solid";
+/* Imports */
+// eslint-disable-next-line no-unused-vars
+import { items } from './movies.json';
+// eslint-disable-next-line no-unused-vars
+import { StarIcon } from '@heroicons/vue/24/solid';
 </script>
 
 <template>
   <!-- This is where your template goes	-->
-  <div></div>
+  <div />
 </template>
