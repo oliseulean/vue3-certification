68014a5ce361739b07a047c7ec93a9c4d90385db
oliseulean <seulean.olimpiu@gmail.com>
2023-06-22 11:56:37 +0300

    test.

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@
 import { items } from './movies.json';
 // eslint-disable-next-line no-unused-vars
 import { StarIcon } from '@heroicons/vue/24/solid';
+console.log('test');
 </script>
 
 <template>
